bbb9539a6eccd00a7b88df0e1f2549ed9b024fca
tmontfort <tmontfort@nvidia.com>
2025-10-13 09:53:49 -0700

    tmp fix

--- deploy/cloud/operator/internal/dynamo/backend_vllm.go
+++ deploy/cloud/operator/internal/dynamo/backend_vllm.go
@@ -96,7 +96,13 @@ func updateVLLMMultinodeArgs(container *corev1.Container, role Role, serviceName
 		leaderHostname := multinodeDeployer.GetLeaderHostname(serviceName)
 		dataParallelSizeLocal := getContainerGPUs(container)
 		// nodeRank, _ := multinodeDeployer.GetNodeRank()
-		startRank := fmt.Sprintf("$(( %d * GROVE_PCLQ_POD_INDEX ))", dataParallelSizeLocal)
+		var startRank string
+		switch role {
+		case RoleWorker:
+			startRank = "1"
+		case RoleLeader:
+			startRank = "0"
+		}
 		container.Args = append(container.Args,
 			"--data-parallel-address", leaderHostname,
 			"--data-parallel-size-local", strconv.FormatInt(dataParallelSizeLocal, 10),
